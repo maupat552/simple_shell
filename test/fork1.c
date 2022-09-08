c8b9b2de3eed184b46bb63dc032053222559de69
Mercy Aupat <mercyaupat7@gmail.com>
2022-09-08 09:16:29 +0300

    simple shell

---
+++ test/fork1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - fork example
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    pid_t my_pid;
+    pid_t pid;
+    prf("Beforefork\n");
+    pid = fork();
+ 
+int    if (pid == -1)
+    {
+        perror("Error:");
+        return (1);
+    }
+    printf("After fork\n");
+    my_pid = getpid();
+    printf("My pid is %u\n", my_pid);
+    return (0);
+}
